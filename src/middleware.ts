8870d6316ef89d69a3a5544af5771332f218482b
contributorsambhav <sambhav511974@gmail.com>
2025-04-04 22:09:17 +0530

    updated : restriction mail array

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,30 @@ import { betterFetch } from "@better-fetch/fetch";
 import { NextResponse, type NextRequest } from "next/server";
 import { redirect } from "next/navigation";
 
-const admin_emails = ["chauhananiket2004@gmail.com"] 
+const admin_emails = ["sambhav511974@gmail.com",
+"sharmasaurenb@gmail.com",
+"manavisharma529@gmail.com",
+"pragya.artik@gmail.com",
+"chauhananiket2004@gmail.com",
+"ujjawalmahesh005@gmail.com",
+"anshijain63@gmail.com",
+"junejasoham@gmail.com",
+"ujjwal.kumar4987@gmail.com",
+"23bcs124@nith.ac.in",
+"nandinigusain17@gmail.com",
+"ishaan.ps@gmail.com",
+"rahulyadavjnvshravasti669@gmail.com",
+"rishabhchadha21@gmail.com",
+"sarthakarora108108@gmail.com",
+"bansalishita59@gmail.com",
+"archiarchisman@gmail.com",
+"ananyapratapsingh7@gmail.com",
+"harshitsethi9165@gmail.com",
+"prajapatiarjun68663@gmail.com",
+"lokeshjaiswal772705@gmail.com",
+"harshsaini0107@gmail.com",
+"22bma007@nith.ac.in",
+"varshneyanurag125@gmail.com"] 
 
 export default async function authMiddleware(request: NextRequest) {
   // extracting out full path of the url except base url
